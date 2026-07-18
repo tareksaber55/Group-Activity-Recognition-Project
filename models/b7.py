3bc2a6db5068826ca62d03340ada3a0ac0d4a8f5
Tarek Saber <145221665+tareksaber55@users.noreply.github.com>
2026-07-18 23:41:23 +0300

    Create b7.py

---
+++ models/b7.py
@@ -0,0 +1,64 @@
+import torch
+import torch.nn as nn
+import torchvision
+import torchvision.models as models
+
+class Baseline7(nn.Module):
+    def __init__(self,player_backbone,group_backbone,num_classes = 8):
+        super(Baseline7,self).__init__()
+        self.cnn = player_backbone.cnn
+        self.lstm1 = player_backbone.lstm
+        self.lstm2 = group_backbone.lstm
+        for param in self.cnn.parameters():
+            param.requires_grad = False
+        for param in self.lstm1.parameters():
+            param.requires_grad = False
+        self.player_proj = nn.Sequential(
+            nn.Linear(3072,1024),
+            nn.ReLU(),
+            nn.Dropout(0.3)
+        )
+        self.classifier = nn.Sequential(
+            nn.Linear(1024,512),
+            nn.BatchNorm1d(512),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            nn.Linear(512,num_classes)
+        )
+
+    def train(self,mode=True):
+        super().train(mode)
+        self.cnn.eval()
+        return self
+    
+
+    def forward(self, x):
+        B,F,P,C,H,W = x.shape
+
+        x = x.view(B*F*P,C,H,W)
+
+        cnn_out = self.cnn(x)
+        cnn_out = torch.flatten(cnn_out,1)
+        cnn_out = cnn_out.view(B,F,P,2048)
+
+        lstm_input = cnn_out.permute(0,2,1,3)
+        lstm_input = lstm_input.reshape(B*P,F,2048)
+
+        lstm1_out,_ = self.lstm1(lstm_input)
+
+        lstm1_out = lstm1_out.reshape(B,P,F,1024)
+        lstm1_out = lstm1_out.permute(0,2,1,3)
+
+        person_features = torch.cat([cnn_out,lstm1_out],dim=-1)
+
+        person_features,_ = torch.max(person_features,dim=2)
+
+        person_features = self.player_proj(person_features)
+
+        lstm2_out,_ = self.lstm2(person_features)
+
+        lstm2_out = lstm2_out[:,-1,:]
+
+        return self.classifier(lstm2_out)
+
+
