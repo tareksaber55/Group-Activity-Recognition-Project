b914866e0a5309375afb388ac501e83550d595d1
Tarek Saber <145221665+tareksaber55@users.noreply.github.com>
2026-07-22 23:03:35 +0300

    create baseline 8

---
+++ models/b8.py
@@ -0,0 +1,138 @@
+import torch
+import torch.nn as nn
+import torchvision
+import torchvision.models as models
+import torch
+import torch.nn as nn
+
+class Baseline8(nn.Module):
+    def __init__(self, player_backbone, image_backbone, num_classes=8):
+        super().__init__()
+
+        # Frozen backbones
+        self.image_cnn = nn.Sequential(
+            *list(image_backbone.model.children())[:-1]
+        )
+        self.player_cnn = player_backbone.cnn
+        self.player_lstm = player_backbone.lstm
+
+        self.lstm2 = nn.LSTM(
+            input_size=2048,
+            hidden_size=1024,
+            num_layers=1,
+            batch_first=True
+        )
+
+        for module in [self.image_cnn, self.player_cnn, self.player_lstm]:
+            for param in module.parameters():
+                param.requires_grad = False
+
+        # Player feature projection
+        self.player_proj = nn.Sequential(
+            nn.Linear(3072, 2048),
+            nn.BatchNorm1d(2048),
+            nn.ReLU(),
+            nn.Dropout(0.3)
+        )
+
+        # Fusion projection
+        self.combine_proj = nn.Sequential(
+            nn.Linear(4096, 2048),
+            nn.BatchNorm1d(2048),
+            nn.ReLU(),
+            nn.Dropout(0.3)
+        )
+
+        self.classifier = nn.Sequential(
+            nn.Linear(1024, 512),
+            nn.BatchNorm1d(512),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            nn.Linear(512, num_classes)
+        )
+
+    def train(self, mode=True):
+        super().train(mode)
+        self.image_cnn.eval()
+        self.player_cnn.eval()
+        self.player_lstm.eval()
+        return self
+
+    def forward(self, x_player, x_image):
+
+        # x_player : (B,F,P,C,H,W)
+        # x_image  : (B,F,C,H,W)
+
+        B, F, P, C, H, W = x_player.shape
+
+        with torch.no_grad():
+
+            # Image branch
+            image = x_image.view(B * F, C, H, W)
+            image = self.image_cnn(image)
+            image = torch.flatten(image, 1)
+            image = image.view(B, F, 2048)
+
+            # Player branch
+            player = x_player.view(B * F * P, C, H, W)
+            player = self.player_cnn(player)
+            player = torch.flatten(player, 1)
+            player = player.view(B, F, P, 2048)
+
+            lstm_input = (
+                player.permute(0, 2, 1, 3)
+                      .contiguous()
+                      .view(B * P, F, 2048)
+            )
+
+        # Person temporal modeling 
+        player_temp, _ = self.player_lstm(lstm_input)
+
+        player_temp = (
+            player_temp.view(B, P, F, 1024)
+                       .permute(0, 2, 1, 3)
+                       .contiguous()
+        )
+
+        # Static + Temporal player features
+        player_features = torch.cat([player, player_temp],dim=-1) # (B,F,P,3072)                                  
+
+        # Aggregate players
+
+        left_player_features = player_features[:,:,:6,:].max(dim=2)
+
+        right_player_features = player_features[:,:,6:,:].max(dim=2)
+
+        player_features = torch.cat([left_player_features,right_player_features] , dim=2)
+
+        player_features = player_features.view(B * F , 3072)
+        player_features = self.player_proj(player_features)
+        player_features = player_features.view(B, F, 2048)
+
+        
+
+        # image + players
+        fusion = torch.cat( [image, player_features] ,dim=-1) # (B,F,4096)
+
+        fusion = fusion.view(B * F, 4096)
+        fusion = self.combine_proj(fusion)
+        fusion = fusion.view(B, F, 2048)
+
+        # Group temporal modeling
+        out, _ = self.lstm2(fusion)
+
+        out = out[:, -1, :]
+
+        return self.classifier(out)
+
+
+
+
+        
+
+
+
+
+
+
+
---
+++ train/b8.py
@@ -0,0 +1,209 @@
+import yaml
+from scripts.B8.b8_train import train 
+from scripts.B8.b8_eval import evaluate
+from scripts.test_report import report
+from utils.dataset import PlayerGroupDataset
+from models.b1 import Baseline1
+from models.b5 import B5PlayerClassifier
+from models.b8 import Baseline8
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torchvision import transforms
+from torch.utils.data import DataLoader
+import os
+import shutil
+
+
+# select config path
+config_path = 'configs/b7_config1.yaml'
+
+with open(config_path,'r') as f:
+    config_dict = yaml.safe_load(f)
+
+
+# device
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# backbone
+with open(config_dict['train']['player_backbone'],'rb') as f:
+    backbone_dict =  torch.load(f,map_location=device)
+
+player_backbone = B5PlayerClassifier().to(device)
+player_backbone.load_state_dict(state_dict=backbone_dict['model_state_dict'])
+
+with open(config_dict['train']['image_backbone'],'rb') as f:
+    backbone_dict =  torch.load(f,map_location=device)
+
+image_backbone = Baseline1().to(device)
+image_backbone.load_state_dict(state_dict=backbone_dict['model_state_dict'])
+
+# model
+model = Baseline8(player_backbone=player_backbone,
+                  image_backbone=image_backbone,
+                  num_classes=len(config_dict['dataset']['group_classes'])
+                  ).to(device)
+
+if torch.cuda.device_count() > 1:
+    print(f"Using {torch.cuda.device_count()} GPUs")
+    model = nn.DataParallel(model)
+
+
+# optimizer
+optimizer_name = config_dict['train']['optimizer']['type']
+lr = config_dict['train']['optimizer']['lr']
+weight_decay = config_dict['train']['optimizer']['weight_decay']
+
+if optimizer_name == 'adamw':
+    optimizer = optim.AdamW(
+        filter(lambda p: p.requires_grad, model.parameters()),
+        lr=lr,
+        weight_decay=float(weight_decay)
+    )
+else :
+    optimizer = optim.SGD(
+        filter(lambda p: p.requires_grad, model.parameters()),
+        lr=lr,
+        weight_decay=float(weight_decay),
+        momentum=0.9
+    )
+
+# epochs
+epochs = config_dict['train']['epochs']
+
+
+scheduler_name = config_dict['train']['scheduler']['type']
+if scheduler_name == 'reduce_on_plateau':
+    factor = config_dict['train']['scheduler']['factor']
+    patience = config_dict['train']['scheduler']['patience']
+    scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer,patience=patience,factor=factor)
+elif scheduler_name == 'cosine_annealing':
+    scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer,T_max=epochs)
+else:
+    scheduler = None
+
+# criterion
+label_smoothing = config_dict['train']['label_smoothing']
+criterion = nn.CrossEntropyLoss(ignore_index=-1,label_smoothing=label_smoothing)
+
+
+
+
+# dataset
+input_root = config_dict['dataset']['root']
+annot_file = config_dict['dataset']['annot_file']
+train_ids = config_dict['dataset']['splits']['train']
+val_ids = config_dict['dataset']['splits']['val']
+test_ids = config_dict['dataset']['splits']['test']
+player_dict = config_dict['dataset']['player_classes']
+group_dict = config_dict['dataset']['group_classes']
+
+# image has a lot of space around objects. Let's crop around
+# Do NOT use RandomHorizontalFlip , classes contain direction : l-pass , r-pass
+
+player_train_transform = transforms.Compose([
+    transforms.Resize((224,224)),
+
+
+    transforms.RandomApply([
+        transforms.GaussianBlur(kernel_size=3, sigma=(0.1, 1.0))
+    ], p=0.2),
+    
+    transforms.ToTensor(),
+
+    transforms.Normalize(
+        mean=[0.485, 0.456, 0.406],
+        std=[0.229, 0.224, 0.225]
+    )
+])
+
+player_val_transform = transforms.Compose([
+    transforms.Resize((224,224)),
+    transforms.ToTensor(),
+    transforms.Normalize(
+        mean=[0.485, 0.456, 0.406],
+        std=[0.229, 0.224, 0.225]
+    )
+])
+
+
+
+image_train_transform = transforms.Compose([
+    transforms.Resize((256,256)),
+    transforms.RandomCrop((224, 224)),
+
+    transforms.RandomApply([
+        transforms.GaussianBlur(kernel_size=3, sigma=(0.1, 1.0))
+    ], p=0.2),
+    
+    transforms.ToTensor(),
+
+    transforms.Normalize(
+        mean=[0.485, 0.456, 0.406],
+        std=[0.229, 0.224, 0.225]
+    )
+])
+
+image_val_transform = transforms.Compose([
+    transforms.Resize((256,256)),
+    transforms.CenterCrop((224, 224)),
+    transforms.ToTensor(),
+    transforms.Normalize(
+        mean=[0.485, 0.456, 0.406],
+        std=[0.229, 0.224, 0.225]
+    )
+])
+
+
+train_dataset = PlayerGroupDataset(input_root=input_root,
+                                   annot_pkl_path=annot_file,
+                                   player_dict=player_dict,
+                                   group_dict=group_dict,
+                                   videos_ids=train_ids,
+                                   preprocess=player_train_transform,
+                                   one_frame=False,
+                                   scene_preprocess=image_train_transform
+                                   )
+val_dataset = PlayerGroupDataset(input_root=input_root,
+                                   annot_pkl_path=annot_file,
+                                   player_dict=player_dict,
+                                   group_dict=group_dict,
+                                   videos_ids=val_ids,
+                                   preprocess=player_val_transform,
+                                   one_frame=False,
+                                   scene_preprocess=image_val_transform
+                                   )
+test_dataset = PlayerGroupDataset(input_root=input_root,
+                                   annot_pkl_path=annot_file,
+                                   player_dict=player_dict,
+                                   group_dict=group_dict,
+                                   videos_ids=test_ids,
+                                   preprocess=player_val_transform,
+                                   one_frame=False,
+                                   scene_preprocess=image_val_transform
+                                   )
+
+num_workers = config_dict['train']['num_workers']
+pin_memory = config_dict['train']['pin_memory']
+
+train_loader = DataLoader(train_dataset,batch_size=config_dict['train']['batch_size']['train'],shuffle=True,num_workers=num_workers,pin_memory=pin_memory)
+val_loader = DataLoader(val_dataset,batch_size=config_dict['train']['batch_size']['val'],shuffle=False,num_workers=num_workers,pin_memory=pin_memory)
+test_loader = DataLoader(test_dataset,batch_size=config_dict['train']['batch_size']['val'],shuffle=False,num_workers=num_workers,pin_memory=pin_memory)
+
+# output_path
+output_path = os.path.join(config_dict['experiment']['output_dir'],
+                           config_dict['experiment']['name'])
+os.makedirs(output_path, exist_ok=True)
+
+train(model,optimizer,criterion,train_loader,val_loader,epochs,scheduler,device,output_path)
+shutil.copy(config_path, os.path.join(output_path, "config.yaml"))
+
+checkpoint_dict = torch.load(os.path.join(output_path,'checkpoints','checkpoint.pth'),map_location=device)
+if isinstance(model, nn.DataParallel):
+    model.module.load_state_dict(checkpoint_dict['model_state_dict'])
+else:
+    model.load_state_dict(checkpoint_dict['model_state_dict'])
+_,_,_,all_labels,all_preds =  evaluate(model,test_loader,criterion,device)
+final_report = report(all_labels,all_preds,team_level=True)
+final_report.make_report(output_path)
+
