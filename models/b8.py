8f884160b1e14807eb0e9ede0d4422ab5b9da3d2
Tarek Saber <145221665+tareksaber55@users.noreply.github.com>
2026-07-24 18:47:19 +0300

    Update b8.py

--- models/b8.py
+++ models/b8.py
@@ -12,40 +12,43 @@ class Baseline8(nn.Module):
         self.player_cnn = player_backbone.cnn
         self.player_lstm = player_backbone.lstm
 
-        for module in [self.image_cnn, self.player_cnn, self.player_lstm]:
-            for param in module.parameters():
-                param.requires_grad = False
-
-        # Static image projection (2048 -> 512)
-        self.image_proj = nn.Sequential(
-            nn.Linear(2048, 512),
-            nn.BatchNorm1d(512),
-            nn.ReLU(),
-            nn.Dropout(0.3)
-        )
-
-        # Static player projection (2048 -> 512)
-        self.player_proj = nn.Sequential(
-            nn.Linear(2048, 512),
-            nn.BatchNorm1d(512),
-            nn.ReLU(),
-            nn.Dropout(0.3)
-        )
-
-        # Group temporal modeling
         self.lstm2 = nn.LSTM(
-            input_size=3584,      # 1536 + 1536 + 512
-            hidden_size=512,
+            input_size=2048,
+            hidden_size=1024,
             num_layers=1,
             batch_first=True
         )
 
+        for module in [self.image_cnn, self.player_cnn, self.player_lstm]:
+            for param in module.parameters():
+                param.requires_grad = False
+
+        # Player feature projection
+        self.player_proj = nn.Sequential(
+            nn.Linear(6144, 3072),
+            nn.BatchNorm1d(3072),
+            nn.ReLU(),
+            nn.Dropout(0.2),
+            nn.Linear(3072, 2048),
+            nn.BatchNorm1d(2048),
+            nn.ReLU(),
+            nn.Dropout(0.2)
+        )
+
+        # Fusion projection
+        self.combine_proj = nn.Sequential(
+            nn.Linear(4096, 2048),
+            nn.BatchNorm1d(2048),
+            nn.ReLU(),
+            nn.Dropout(0.3)
+        )
+
         self.classifier = nn.Sequential(
-            nn.Linear(512, 128),
-            nn.BatchNorm1d(128),
+            nn.Linear(1024, 512),
+            nn.BatchNorm1d(512),
             nn.ReLU(),
             nn.Dropout(0.5),
-            nn.Linear(128, num_classes)
+            nn.Linear(512, num_classes)
         )
 
     def train(self, mode=True):
@@ -64,16 +67,16 @@ class Baseline8(nn.Module):
 
         with torch.no_grad():
 
-
+            # Image branch
             image = x_image.view(B * F, C, H, W)
             image = self.image_cnn(image)
             image = torch.flatten(image, 1)
+            image = image.view(B, F, 2048)
 
-
+            # Player branch
             player = x_player.view(B * F * P, C, H, W)
             player = self.player_cnn(player)
             player = torch.flatten(player, 1)
-
             player = player.view(B, F, P, 2048)
 
             lstm_input = (
@@ -82,48 +85,43 @@ class Baseline8(nn.Module):
                       .view(B * P, F, 2048)
             )
 
-
+        # Person temporal modeling 
         player_temp, _ = self.player_lstm(lstm_input)
 
         player_temp = (
             player_temp.view(B, P, F, 1024)
+                       .permute(0, 2, 1, 3)
                        .contiguous()
         )
 
+        # Static + Temporal player features
+        player_features = torch.cat([player, player_temp],dim=-1) # (B,F,P,3072)                                  
 
-        image = self.image_proj(image)
-        image = image.view(B, F, 512)
+        # Aggregate players
 
-        player_static = self.player_proj(player.view(B * F * P, 2048))
-        player_static = player_static.view(B, F, P, 512)
-        player_static = player_static.permute(0, 2, 1, 3)
+        left_player_features,_ = player_features[:,:,:6,:].max(dim=2)
 
+        right_player_features,_ = player_features[:,:,6:,:].max(dim=2)
 
-        player_features = torch.cat(
-            [player_static, player_temp],
-            dim=-1
-        )                               # (B,P,F,1536)
+        player_features = torch.cat([left_player_features,right_player_features] , dim=2)
 
+        player_features = player_features.view(B * F , 3072)
+        player_features = self.player_proj(player_features)
+        player_features = player_features.view(B, F, 2048)
 
-        left_features, _ = player_features[:, :6].max(dim=1)
-        right_features, _ = player_features[:, 6:].max(dim=1)
+        
 
-        # (B,F,3072)
-        player_features = torch.cat(
-            [left_features, right_features],
-            dim=-1
-        )
+        # image + players
+        fusion = torch.cat( [image, player_features] ,dim=-1) # (B,F,4096)
 
+        fusion = fusion.view(B * F, 4096)
+        fusion = self.combine_proj(fusion)
+        fusion = fusion.view(B, F, 2048)
 
-        fusion = torch.cat(
-            [player_features, image],
-            dim=-1
-        )                               # (B,F,3584)
-
-
+        # Group temporal modeling
         out, _ = self.lstm2(fusion)
 
-        out = out[:, -1]
+        out = out[:, -1, :]
 
         return self.classifier(out)
 
